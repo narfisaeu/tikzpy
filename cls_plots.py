4807fdc1cfd587e3699585a6af03beee687b7eab
francisco <francisco>
2019-01-16 12:00:24 +0100

    Plots add

--- cls_plots.py
+++ cls_plots.py
@@ -1,145 +1,185 @@
+#!/usr/bin/python
+
 import math
 import numbers
 import obj_data
 import copy
 
-import assemblies.cls_racime as asse_rac
+import plots.cls_racime as asse_rac
 
 def log(txt):
-    
+
     print txt
 
-class _assemblys(object):
+class _plots(object):
+
+    """**Plots class:**
+
+    .. _plots_cls:
+
+    :platform: Unix, Windows
+    :synopsis: buld-in plots based on pyTikZ
+
+    :properties:
+        Available plots:
+            * Racime
+
+    **Chracteristics of a shape (shp) object**
+
+        * Each shape object has different properties depending on the nature of the shape
+
+    """
 
     def __init__(self, parent):
-    
+
         self.parent = parent
-        
+
         ### Counters
         self.counters = obj_data._clsdata(type = int(0))
-        
+
         ### Dictionary of lines
         self.assemblys = obj_data._clsdata(type = {})
-        self.counters["assemblys"] = 0    
-        
+        self.counters["assemblys"] = 0
+
     def __getitem__(self, key):
-    
-        return self.getitem(key)        
-        
+
+        return self.getitem(key)
+
     def keys(self):
-        
+
         return self.assemblys.keys
-        
-    def getitem(self, key):        
-        
+
+    def getitem(self, key):
+
         ### Give a point
         _key = str(key)
         if self.assemblys[_key]:
             return _assembly(self,_key, self.assemblys[_key]["action"])
         else:
-            return None            
+            return None
     ##########################################
     def racime(self, group = 0):
-        
+
+        """
+        .. _racime_plot:
+
+        **Synopsis:**
+            * Returns a racime plot object
+
+        **Args:**
+            * None
+
+        **Optional parameters:**
+            * group = 0: group id
+
+        **Returns:**
+            * A racime plot object
+
+        **Usage**
+
+            * See :doc:`plots examples </_examples/pytikZ_plots/test_gen>`
+
+        """
+
         rac = self._additem("racime", group = group)
-                
+
         rac._handler.load_data_ini(rac)
-        
+
         return rac
-    
-    ##########################################    
+
+    ##########################################
     def _additem(self, type, group = 0):
-        
+
         # Create auto new
         _key =  "rac#%i" % self.counters["assemblys"]
         self.counters["assemblys"] = self.counters["assemblys"] + 1
         self.assemblys[_key] = {}
-        
+
         lline = _assembly(self,_key, type)
         lline.group = group
         lline.action = type
         lline.addlabel = "default"
         lline.group_label = "##group##" + _key
         return lline
-    
+
     #########################
     @property
     def labels(self):
         return self.parent.lbl.labels
-    
+
     @property
     def addlabel(self):
         return self.labels
-        
+
     @addlabel.setter
     def addlabel(self, value):
-        self.parent.lbl.addlabel = value        
-            
+        self.parent.lbl.addlabel = value
+
     def dellabel(self, value):
         if value in self.labels:
-            
+
             ### Delete from all the assemblys
             for key in self.keys():
-                shp = self.getitem(key)             
+                shp = self.getitem(key)
                 shp.dellabel(value)
-            
+
             ### Remake list
             self.parent.lbl._dellabel(value)
-            
+
             return True
         else:
-            #log("The label %s has not been previously declared" % value)   
+            #log("The label %s has not been previously declared" % value)
             return False
 
     def rename_label(self, name_old, name_new):
-        if name_old != name_new:            
-                        
+        if name_old != name_new:
+
             ### Add new name
             self.addlabel=name_new
-            
+
             ### Delete from all the assemblys
             for key in self.keys():
-                shp = self.getitem(key)             
+                shp = self.getitem(key)
                 if name_old in self.assemblys[key]["labels"]:
                     shp.dellabel(name_old)
                     shp.addlabel=name_new
-            
+
             ### Remake list
             self.parent.lbl._dellabel(name_old)
-            
+
             return True
         else:
-            #log("Inconsistent")            
+            #log("Inconsistent")
             return False
-            
+
 class _assembly(object):
-   
+
     def __init__(self, parent, key, type):
-        
+
         self.parent = parent
         self._key = key
         self._draw = False
-        
+
         if not self.parent.assemblys[key]:
-            
+
             self.parent.assemblys[self._key]["action"] = ""
             self.parent.assemblys[self._key]["group"] = 0
             self.parent.assemblys[self._key]["z-order"] = 0.
             self.parent.assemblys[self._key]["labels"] = []
             self.parent.assemblys[self._key]["group_label"] = ""
             self.parent.assemblys[self._key]["mpto"] = None
-            
+
         if type == "racime":
             self._handler = asse_rac._racime(self.parent)
         else:
-            raise Error            
-        
+            raise Error
+
         #Setter and getter properties dynamically add
         for ele in self._handler.lst_data:
-            self.addProperty(ele)        
-        
+            self.addProperty(ele)
+
     def addProperty(self, attribute):
-        # create local setter and getter with a particular attribute name 
+        # create local setter and getter with a particular attribute name
         getter = lambda self: self._getProperty(attribute)
         setter = lambda self, value: self._setProperty(attribute, value)
 
@@ -147,130 +187,130 @@ class _assembly(object):
         setattr(self.__class__, attribute, property(fget=getter, \
                                                     fset=setter, \
                                                     doc="Auto-generated method"))
-                                                    
+
     def _setProperty(self, attribute, value):
         #print "Setting: %s = %s" %(attribute, value)
         #self.parent.assemblys[self._key][attribute] = value
         self._handler.set_property(attribute, value, self._key)
-        
+
     def _getProperty(self, attribute):
         #print "Getting: %s" %attribute
         #return self.parent.assemblys[self._key][attribute]
         return self._handler.get_property(attribute, self._key)
-        
+
     #############################################
-    
+
     def copy(self):
-        
+
         ### To change
-        
+
         shp = self.parent._additem(self.action, group = self.group)
-        
+
         for key in self.parent.assemblys[self.id].keys():
-            
+
             self.parent.assemblys[shp.id][key] = copy.deepcopy(self.parent.assemblys[self.id][key])
-           
+
     def move(self, mpto):
         ### Move function
         self.parent.assemblys[self._key]["mpto"] = mpto.copy()
-    
+
     def draw_group_elements(self, asse, units = ""):
-        
+
         shps = self._handler.draw_group_elements(units, asse)
         if type(shps) != type([]):
             log("Error draw_group_elements function definition")
-        
+
         ### Common practices
         for shp in shps:
             # Edit zorder
             zorder = shp.zorder
             zorder = (zorder / 10000.) + self.parent.assemblys[self._key]["z-order"]
             shp.zorder = zorder
-            
+
             # Add common group label
             shp.group_label = self.parent.assemblys[self._key]["group_label"]
-            
+
             # Add labels
             for lbl in self.parent.assemblys[self._key]["labels"]:
                 shp.addlabel = lbl
-            
+
         ### Move
         mpto = self.parent.assemblys[self._key]["mpto"]
-        if mpto:
-            self.parent.parent.move_list_shapes( mpto, shps)
-        
+        #if mpto:
+        #    self.parent.parent.move_list_shapes( mpto, shps)
+
         # Add labels
         for lbl in self.parent.assemblys[self._key]["labels"]:
-            self.parent.parent.add_label_to_list_shapes(shps, lbl)           
-        
+            self.parent.parent.add_label_to_list_shapes(shps, lbl)
+
     #############################################
-    
+
     def add_element(self, text, thickness = "thin", separation = None):
-                
+
         self.parent.assemblys[self._key]["element"].append([text, thickness, separation])
-    
+
     '''
     @property
     def l1(self):
-    
+
         return self.parent.assemblys[self._key]["l1"]
-        
+
     @l1.setter
     def l1(self, value):
-        
-        self.parent.assemblys[self._key]["l1"] = value   
+
+        self.parent.assemblys[self._key]["l1"] = value
     '''
     #############################################
-    
+
     @property
     def id(self):
-        return self._key      
-    
+        return self._key
+
     @property
     def action(self):
-    
+
         return self.parent.assemblys[self._key]["action"]
-        
+
     @action.setter
     def action(self, value):
-        
+
         self.parent.assemblys[self._key]["action"] = value
-    
+
     @property
     def group(self):
         return self.parent.assemblys[self._key]["group"]
-        
+
     @group.setter
     def group(self, value):
         self.parent.assemblys[self._key]["group"] = int(value)
-        
+
     @property
     def zorder(self):
         return self.parent.assemblys[self._key]["z-order"]
-        
+
     @zorder.setter
     def zorder(self, value):
-        self.parent.assemblys[self._key]["z-order"] = float(value)  
+        self.parent.assemblys[self._key]["z-order"] = float(value)
 
     @property
     def _labels(self):
         return self.parent.assemblys[self._key]["labels"]
-        
+
     @property
     def labels(self):
         return copy.deepcopy(self.parent.assemblys[self._key]["labels"])
-       
+
     @property
     def addlabel(self):
         return self.labels
-        
+
     @addlabel.setter
     def addlabel(self, value):
-        
+
         if type(value) is type([]):
             for val in value:
                 self.addlabel = val
-        else:    
+        else:
             if value in self.parent.labels:
                 if not value in self.parent.assemblys[self._key]["labels"]:
                     self.parent.assemblys[self._key]["labels"].append(value)
@@ -280,7 +320,7 @@ class _assembly(object):
                 if not value in self.parent.assemblys[self._key]["labels"]:
                     self.parent.assemblys[self._key]["labels"].append(value)
                 return True
-            
+
     def dellabel(self, value):
         if value in self.parent.labels:
             if value in self.parent.assemblys[self._key]["labels"]:
@@ -288,25 +328,25 @@ class _assembly(object):
                 self.parent.assemblys[self._key]["labels"] = newlst
                 return True
             else:
-                #log("The label %s has not been added before" % value)            
+                #log("The label %s has not been added before" % value)
                 return False
         else:
-            #log("The label %s has not been previously declared" % value)            
+            #log("The label %s has not been previously declared" % value)
             return False
-                
+
     def __repr__(self):
         return self.__class__
-        
+
     def __str__(self):
-        
-        return "assembly key:%s group=%i Num assemblys:%i" % (self.id, self.group, len(self.parent.assemblys))                
-    
+
+        return "assembly key:%s group=%i Num assemblys:%i" % (self.id, self.group, len(self.parent.assemblys))
+
     @property
     def grouplabel(self):
         return self.parent.assemblys[self._key]["group_label"]
-        
+
     @group.setter
     def grouplabel(self, value):
-        self.parent.assemblys[self._key]["group_label"] = int(value)    
-        
-    #############################################           
+        self.parent.assemblys[self._key]["group_label"] = int(value)
+
+    #############################################
--- plots/cls_racime.py
+++ plots/cls_racime.py
@@ -4,47 +4,47 @@ import pyTikZ.obj_data
 import copy
 
 def log(txt):
-    
+
     print txt
 
 class _racime(object):
 
     def __init__(self, parent):
-        
+
         self._parent = parent
-        self._tik = self._parent.parent         
-        
+        self._tik = self._parent.parent
+
         ### List of extra data properties
         self.lst_data = ["element", "l1", "l2", "l3", "origin", "separation", "p_vertex", "p_racimes", "end_thickness", "total_height"]
-        
+
     def load_data_ini(self, assem):
         ### Initialize properties
         _key = assem.id
-                
+
         self._parent.assemblys[_key]["element"] = []
         self._parent.assemblys[_key]["l1"] = None
         self._parent.assemblys[_key]["l2"] = None
         self._parent.assemblys[_key]["l3"] = None
         self._parent.assemblys[_key]["origin"] = None
         self._parent.assemblys[_key]["separation"] = 1.
-    
+
     def set_property(self, attribute, value, _key):
         ### Properties setter bottleneck
-        
+
         if attribute == "p_vertex":
             pass
         elif attribute == "p_racimes":
-            pass        
+            pass
         elif attribute == "end_thickness":
-            pass        
+            pass
         elif attribute == "total_height":
             pass
         else:
             self._parent.assemblys[_key][attribute] = value
-    
+
     def get_property(self, attribute, _key):
         ### Properties getter bottleneck
-        
+
         if attribute == "p_vertex":
             [p1,p2,p3,p4,pm,th_si,sum_si] = self._calculation_points(_key)
             out = []
@@ -57,15 +57,15 @@ class _racime(object):
             for p in p1:
                 out.append(p + self._parent.assemblys[_key]["mpto"])
             return out
-        elif attribute == "total_height":            
+        elif attribute == "total_height":
             [p1,p2,p3,p4,pm,th_si,sum_si] = self._calculation_points(_key)
             return sum_si
         elif attribute == "end_thickness":
             [p1,p2,p3,p4,pm,th_si,sum_si] = self._calculation_points(_key)
-            return th_si            
+            return th_si
         else:
             return self._parent.assemblys[_key][attribute]
-        
+
     def draw_group_elements(self, units, assem):
         ### Draw group of shapes function call from main drawer functions
         ### Common heander
@@ -73,53 +73,53 @@ class _racime(object):
         _key = assem.id
         shps = []
         ##
-        
+
         # Calculate points
         [p1,p2,p3,p4,pm,th_si,sum_si] = self._calculation_points(_key)
-        
+
         # Loop elements
         for i in range(0,len(p1)):
-            
+
             [text, thickness, separation] = self._parent.assemblys[_key]["element"][i]
-            
+
             # Add lines
-            l1 = tik.shp.line(0)        
+            l1 = tik.shp.path([], layer = 0)
             l1.addpto = p1[i]
             l1.addpto = p2[i]
             l1.addpto = p3[i]
-            
-            if thickness: l1.thick = thickness   
+
+            if thickness: l1.thick = thickness
             shps.append(l1)
-            
+
             l = tik.shp.text(pm[i], text,0)
             l.align = 0
-            l.position = "above"            
+            l.position = "above"
             shps.append(l)
-            
-        l2 = tik.shp.line(0)  
+
+        l2 = tik.shp.path([], layer = 0)
         l2.addpto = p3[0]
         l2.addpto = p4[0]
-        
-        l2.thick = th_si 
+
+        l2.thick = th_si
         shps.append(l2)
-            
+
         ### Common return
         return shps
-        
+
     def _calculation_points(self, key):
-        
+
         #   p1-----------p2
         #          s1      \
         #   p1-----------p2-p3---p4
         #          s2      /
-        #   p1-----------p2        
+        #   p1-----------p2
         #          l1      l2  l3
         ### Common heander
         tik = self._tik
         _key = key
         ptos = []
-        ##              
-                
+        ##
+
         ## Looop elements
         i = 0
         sum_si = 0.
@@ -129,58 +129,57 @@ class _racime(object):
         p3 = []
         p4 = []
         pm = []
-        
+
         for ele in self._parent.assemblys[_key]["element"]:
             # Data
             [text, thickness, separation] = ele
-            
+
             # Points
             _p1 = tik.pto.aux(0.,sum_si,0.)
             p1.append(_p1)
-            
+
             _p2 = tik.pto.aux(float(self._parent.assemblys[_key]["l1"]),sum_si,0.)
-            p2.append(_p2)   
+            p2.append(_p2)
 
             _pm = tik.pto.aux(float(self._parent.assemblys[_key]["l1"])/2.,sum_si,0.)
-            pm.append(_pm)              
-            
+            pm.append(_pm)
+
             # Max height
-            if i < N - 1:                     
+            if i < N - 1:
                 if separation is None:
                     sum_si = self._parent.assemblys[_key]["separation"] + sum_si
                 else:
                     sum_si = separation + sum_si
-                                       
+
             i = i + 1
-        
+
         th = 0.
         x = float(self._parent.assemblys[_key]["l1"]) + float(self._parent.assemblys[_key]["l2"])
         xx = x + float(self._parent.assemblys[_key]["l3"])
         y = (sum_si/2.)# - th_si/2.
-        
+
         for ele in self._parent.assemblys[_key]["element"]:
             # Data
-            [text, thickness, separation] = ele        
-            
+            [text, thickness, separation] = ele
+
             if self.is_number(thickness):
                  th = th + float(thickness)
             else:
                  th = th + float(1)
-            
+
             _p3 = tik.pto.aux(x,y,0.)
             _p4 = tik.pto.aux(xx,y,0.)
-            
+
             p3.append(_p3)
             p4.append(_p4)
-        
+
         ptos = [p1,p2,p3,p4,pm,th, sum_si]
-        
+
         return ptos
-        
+
     def is_number(self, s):
         try:
             float(s)
             return True
         except ValueError:
-            return False        
-        
+            return False

b93b9fd36cbe2224ba4c608cc9b72435b17f7c88
narfisaeu <51031976+narfisaeu@users.noreply.github.com>
2019-05-25 22:34:38 +0200

    version 0

---
+++ cls_data.py
@@ -0,0 +1,15 @@
+import cls_points 
+import obj_data
+import copy
+
+class clsdata(object):
+   
+    def __init__(self):
+        
+        self.pto = cls_points._points(self)
+        
+        self.options = obj_data._clsdata(type = {})
+    
+
+   
+    
